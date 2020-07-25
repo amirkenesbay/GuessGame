7f93b7e50aeb96754f592c365d56bb83bf0b98e5
amirkenesbay <amirkenesbay@gmail.com>
2020-07-25 13:11:13 +0600

    Fix some bugs

--- src/Player.java
+++ src/Player.java
@@ -1,12 +1,13 @@
 public class Player {
     int number = 0;
+
     public void guess() {
         number = (int) (Math.random() * 10);
         System.out.println("Думаю, это число " + number);
     }
 }
 
-class GameLauncher{
+class GameLauncher {
     public static void main(String[] args) {
         GuessGame game = new GuessGame();
         game.startGame();
